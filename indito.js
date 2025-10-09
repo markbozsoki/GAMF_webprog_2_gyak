d81a9fa4bff2f3e0ed41f6e6f2bcd9fecd30a679
markbozsoki <bozsoki.mark@icloud.com>
2025-10-10 00:55:35 +0200

    update_server_file

--- indito.js
+++ indito.js
@@ -4,41 +4,42 @@ var http = require('http'),
 var server_port = 8080
 http.createServer(function (request, response) {
     var url = request.url;
-    if (url === '/' || url === '') {
-        response.writeHeader(200, { "Content-Type": "text/html" });
+    console.log(`request url path: ${url}`)
+
+    if (url === '/' || url === '' || url === '/index.html') {
         fs.readFile('./pages/index.html', function (err, html) {
             if (err) {
                 throw err;
             }
             response.write(html);
+            response.end();
         });
-    }
-    if (url === '/admin') {
-        response.writeHeader(200, { "Content-Type": "text/html" });
+    } else if (url === '/admin') {
         fs.readFile('./pages/admin.html', function (err, html) {
             if (err) {
                 throw err;
             }
             response.write(html);
+            response.end();
         });
     } else if (url === '/login') {
-        response.writeHeader(200, { "Content-Type": "text/html" });
         fs.readFile('./pages/login.html', function (err, html) {
             if (err) {
                 throw err;
             }
             response.write(html);
+            response.end();
         });
-    } else {
-        response.writeHeader(404, { "Content-Type": "text/html" });
-        fs.readFile('./pages/404.html', function (err, html) {
+    } else if (url.startsWith('/assets')) {
+        var asset_path = url.split('?')[0]
+        fs.readFile(`.${asset_path}`, function (err, asset) {
             if (err) {
                 throw err;
             }
-            response.write(html);
+            response.write(asset);
+            response.end();
         });
     }
-    response.end();
 }).listen(server_port, function () {
     console.log(`server start at port ${server_port}`)
 });
