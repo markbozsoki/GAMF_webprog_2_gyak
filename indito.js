2ed404259f84e6c855e459c7fb97b2ed97d8ae39
markbozsoki <bozsoki.mark@icloud.com>
2025-10-11 15:15:53 +0200

    add_middleware

--- indito.js
+++ indito.js
@@ -7,6 +7,27 @@ var genuuid = require('uudi/v4');
 const session = require('express-session');
 
 var server_port = 8080
+
+app.use( // session middleware
+    session(
+        {
+            name: 'SessionCookie',
+            genid: function(request) {
+                let session_id = genuuid();
+                console.log(`session id created: ${session_id}`);
+                return session_id;
+            },
+            secret: 'this_is_the_secret',
+            resave: false,
+            saveUninitialized: false,
+            cookie: {
+                secure: false,
+                expires: 600_000 // miliseconds (10 min)
+            }
+        }
+    )
+);
+
 http.createServer(function (request, response) {
     var url = request.url;
     console.log(`request url path: ${url}`)
