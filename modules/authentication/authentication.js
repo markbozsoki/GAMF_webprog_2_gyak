088019e19f23f07dd120334879158f54d3ed69b9
markbozsoki <bozsoki.mark@icloud.com>
2025-10-24 16:30:00 +0200

    apply_env_variables

--- modules/authentication/authentication.js
+++ modules/authentication/authentication.js
@@ -1,9 +1,38 @@
 export const validateUserForLogin = function validateUserForLogin(request_body) {
     console.log('validateing userdata for login');
+    var con = mysql.createConnection({
+            host: process.env.DB_HOST,
+            user: process.env.DB_USERNAME, 
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_DATABASE
+        });
+    
+    con.ping(function (err) {
+        if (err) {
+            console.error(`Cannot connect to: ${databaseName} for user authentication`);
+            return;
+        }
+    });
+
     return true;
 };
 
 export const registerNewUser = function registerNewUser(request_body) {
     console.log('registering new user');
+var con = mysql.createConnection({
+            host: process.env.DB_HOST,
+            user: process.env.DB_USERNAME, 
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_DATABASE
+        });
+    
+    con.ping(function (err) {
+        if (err) {
+            console.error(`Cannot connect to: ${databaseName} for user registration`);
+            return;
+        }
+    });
+    
+
     return true;
 };
