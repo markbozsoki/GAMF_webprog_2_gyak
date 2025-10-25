0df31459f057ea17cd9cd10e1c55a26bc0f688a5
markbozsoki <bozsoki.mark@icloud.com>
2025-10-25 18:24:14 +0200

    add_event_listenre_for_table_load

---
+++ assets/js/messagestable.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var tbody = document.getElementById("messages-table-body");
+    var rows = [];
+
+    fetch("/messages")
+        .then(function (response) {
+            return response.text();
+        })
+        .then(function (html) {
+            tbody.innerHTML = "";
+            var table = document.createElement("table");
+            table.innerHTML = html;
+            rows = Array.from(table.querySelectorAll("tr")).slice(0);
+            rows.forEach(function (row) {
+                tbody.appendChild(row);
+            })
+        })
+        .catch(function (err) {
+            console.error("Hiba a lekérés közben:", err);
+            tbody.innerHTML = "<tr><td colspan='4'>Nem sikerült betölteni az adatokat.</td></tr>";
+        });
+});
